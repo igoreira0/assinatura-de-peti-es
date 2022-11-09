1aebe6bf0335bed85c47a8dd67ac9ef437ab7798
igor <igoreira0@gmail.com>
2022-11-08 21:40:42 -0300

    adding not found route

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 require('./src/controllers/authController')(app);
 require('./src/controllers/peticaoController')(app);
 require('./src/controllers/healthController')(app);
-
+app.use(function(req, res, next) {
+    res.status(404);
+    res.json({status:404,title:"Not Found",msg:"Route not found"});
+    next();
+});
 
 console.log(`listening http://localhost:${port}`)
 app.listen(port);
